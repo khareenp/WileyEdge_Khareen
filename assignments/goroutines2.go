810ff037915b8e0de4cf2b98de269dade566eceb
khareenp <khareenp1@gmail.com>
2022-07-15 13:48:23 -0400

    update on go routines

---
+++ assignments/goroutines2.go
@@ -0,0 +1,46 @@
+//Khareen Proverbs
+//Friday Jun 15
+//go routines
+
+// 2)Take a Paragraphs of text Max word  up to 200 words in a Slice or String
+//  and then Pass each  Sentence(up to full stop) to another go routine/routines
+//  1)print it in reverse order(1 goroutine)
+//  2 )count its words(2 goroutine)
+
+package assignments
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+//takes a string and a channel of array as parameters
+func Send(sample string, ch chan []string){
+	//splits string into slice of sentences using "." as split value
+	res:= strings.Split(sample, ".")
+	for i:= 0; i < len(res); i++{
+		fmt.Println(res[i])
+	}
+	ch <- res // value received
+}
+
+func receive( sample string, ch chan []string){
+	time.Sleep(time.Second *2)
+	res:= <- ch
+	fmt.Println(res)
+
+}
+
+
+func driver2(){
+	st := "If you're looking for random paragraphs, you've come to the right place. When a random word or a random sentence isn't quite enough, the next logical step is to find a random paragraph. We created the Random Paragraph Generator with you in mind. The process is quite simple. Choose the number of random paragraphs you'd like to see and click the button. Your chosen number of paragraphs will instantly appear."
+
+	ch:= make(chan []string)
+
+	go Send(st,ch)
+
+	res:= <-ch
+	fmt.Println(res)
+
+}
---
+++ assignments/goroutines3.go
@@ -0,0 +1,36 @@
+//Khareen Proverbs
+//Friday Jul 15
+
+// 3)Create a goroutine/channels to send 
+// and receive structure data type  free to design for any purpose
+
+
+package assignments
+
+//Demo  of Slice of channels of integer type  package main
+
+import (
+    "fmt"
+)
+
+func demo() {
+    c := make(chan []int)
+
+    things := []int{1, 2, 3}
+
+    go func() {
+        c <- things
+    }()
+
+    for _, i := range <-c {
+        fmt.Println(i)
+    }
+
+    go func() {
+        c <- append(things, 4)
+    }()
+
+    for _, i := range <-c {
+        fmt.Println(i)
+    }
+}
