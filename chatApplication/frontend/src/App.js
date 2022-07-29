f17320f2aeb504830f0f9eea761ee7d91baf895d
khareenp <khareenp1@gmail.com>
2022-07-28 21:24:13 -0400

    update

--- chatApplication/frontend/src/App.js
+++ chatApplication/frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import ChatHistory from "./components/ChatHistory/ChatHistory.jsx";
 import ChatInput from "./components/ChatInput/ChatInput.jsx";
 import { connect, sendMsg } from "./api";
+import { Formik } from "formik";
 
 class App extends Component {
   constructor(props) {
@@ -20,33 +21,10 @@ class App extends Component {
     connect((msg) => {
       console.log("New Message");
       this.setState((prevState) => ({
-        chatHistory: [...this.state.chatHistory, msg],
+        chatHistory: [...prevState.chatHistory, msg],
       }));
       console.log(this.state);
     });
-  }
-
-  // By passing in this event, we’ll be able to query if the key pressed was the Enter key,
-  // if it is, we’ll be able to send the value of our <input/> field
-  // to our WebSocket endpoint and then subsequently clear that <input/>:
-  // send(event) {
-  //   if (event.keyCode === 13) {
-  //     sendMsg(event.target.value);
-  //     event.target.value = "";
-  //   }
-  // }
-  //event handler for "enter" key press
-  textInputHandler(event) {
-    if (event.keyCode === 13) {
-      sendMsg(event.target.value);
-      event.target.value = "";
-    }
-  }
-  //event handler for clicking send
-  clickHandler(_event) {
-    // send message here
-    sendMsg(_event.target.value);
-    _event.target.value = _event;
   }
 
   render() {
@@ -54,15 +32,46 @@ class App extends Component {
       <div className="App flex flex-col w-full">
         <Header />
         <ChatHistory chatHistory={this.state.chatHistory} />
-        <ChatInput send={this.textInputHandler} />
-        <div className="flex mt-2 mr-3 justify-end md:mr-20">
-          <button
-            className="w-1/5 mt-1 text-xl border-2 p-2 bg-slate-300 rounded-xl"
-            onClick={this.clickHandler}
-          >
-            Send
-          </button>
-        </div>
+        {/* formik is a library for form handling */}
+        <Formik
+          initialValues={{ msg: "" }}
+          validateOnBlur={false}
+          validateOnChange={false}
+          onSubmit={(values, actions) => {
+            actions.setSubmitting(false);
+            console.log(values);
+            sendMsg(values.msg);
+            // clears form input field after msg sent
+            actions.resetForm();
+            // onSubmit(values)
+          }}
+        >
+          {({ handleChange, handleSubmit, isSubmitting, values }) => (
+            <>
+              {/* send message on enter key press */}
+              <ChatInput
+                send={(event) => {
+                  if (event.keyCode === 13) {
+                    handleSubmit();
+                  }
+                }}
+                name="msg"
+                value={values.msg}
+                handleChange={handleChange}
+              />
+              <div className="flex mt-2 mr-3 justify-end md:mr-20">
+                {/* submit message on button press */}
+                <button
+                  type="submit"
+                  className="w-1/5 mt-1 text-xl border-2 p-2 bg-slate-300 rounded-xl"
+                  onClick={handleSubmit}
+                >
+                  Send
+                </button>
+              </div>
+            </>
+          )}
+        </Formik>
       </div>
     );
   }
