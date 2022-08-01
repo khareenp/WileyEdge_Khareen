11dc86ce7365f30a9ff75c0107fc00c54c459476
khareenp <khareenp1@gmail.com>
2022-08-01 14:29:31 -0400

    update

--- chatApplication/frontend/src/components/ChatInput/ChatInput.tsx
+++ chatApplication/frontend/src/components/ChatInput/ChatInput.tsx
@@ -1,32 +1,37 @@
-import { FC } from "react";
+// This component will take in an array of chat messages from our App.js function
+// through its' props and will subsequently render them one under the other.
 
-// props
-type ChatInputProps = {
-  name: string;
-  value: string;
-  handleChange: any;
-  handleSubmit: any;
+import { FC } from "react";
+import { ChatMessage } from "../../App";
+import Message from "../Message/Message";
+``;
+type ChatHistoryProps = {
+  chatHistory: ChatMessage[];
+  ip: string;
 };
 
-// simple input to handle chat messages
-const ChatInput: FC<ChatInputProps> = ({
-  name,
-  handleChange,
-  handleSubmit,
-  value,
-}) => {
+const ChatHistory: FC<ChatHistoryProps> = ({ chatHistory, ip }) => {
+  var today = new Date();
+  const date =
+    today.getMonth() + 1 + "-" + today.getDate() + "-" + today.getFullYear();
+
+  // message list
+  const messages = chatHistory.map((msg, index) => (
+    <Message key={index.toString()} message={msg} />
+  ));
+
   return (
-    <div className="ChatInput block ml-6">
-      <input
-        name={name}
-        className=" w-11/12 p-2 text-base rounded shadow-md"
-        onChange={handleChange}
-        value={value}
-        onSubmit={handleSubmit}
-        placeholder="Please type a message, hit enter to send"
-      />
+    <div className="ChatHistory bg-slate-100 text-left h-5/6 md:text-2xl sm:text-xl md:font-semibold tracking-wide">
+      <h2 className=" p-5 bg-sky-200 font-semibold text-xl">
+        Chat History
+        {/* <p>{this.state.currentDateTime}</p> */}
+        <p className=" text-sm">Todays Date: {date}</p>
+        <p className=" text-sm">My IP Address: {ip}</p>
+      </h2>
+      {/* prop for message data */}
+      {messages}
     </div>
   );
 };
 
-export default ChatInput;
+export default ChatHistory;
