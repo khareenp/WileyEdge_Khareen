4a27de64f4aaaa907bfab1139332e083b1cee73d
khareenp <khareenp1@gmail.com>
2022-08-03 11:23:45 -0400

    change header text

--- chatApplication/frontend/src/components/Header/Header.tsx
+++ chatApplication/frontend/src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 
 const Header = () => (
   <div className="header flex w-full bg-sky-800 p-4 text-slate-50 font-bold tracking-wide">
-    <h2 className="md:text-4xl sm:text-2xl flex-1">Proverbs Talks App</h2>
+    <h2 className="md:text-4xl sm:text-2xl flex-1">Chat App</h2>
   </div>
 );
 
